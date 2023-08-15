4c27e4d10b3fc601a7c22b6ddd5c76948587bcab
Akash Dev <akash.dev.8092@gmail.com>
2023-08-15 23:01:09 +0530

    new projects

---
+++ guessTheNumber/main.py
@@ -0,0 +1,34 @@
+import random
+from art import logo
+
+print(logo)
+
+def guessed():
+    if level == "hard":
+        chance = 5
+    elif level == "easy":
+        chance = 10
+    
+    while chance != 0:
+        yourguess = int(input("Enter your guess: "))
+        checkGuess(yourguess, guess)
+        if yourguess == guess:
+            return True
+        chance -= 1
+        print(f"Wrong answer.. You have {chance} chances left")
+    
+    return False
+def checkGuess(myGuess, guess):
+    if(myGuess>guess):
+        print("Your guess is too high guess a short number")
+    elif(myGuess<guess):
+        print("Your guess is too low please guess a bigger number")
+    
+level = input("Please select your level: easy or hard\n")
+guess = random.randint(0, 100)
+print (guess)
+
+if guessed():
+    print("Congratulations! You guessed the correct number.")
+else:
+    print("Sorry, you didn't guess the correct number.")
