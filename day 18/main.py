a72bf7a2fb66909989782a4162b417b69993a0ce
Akash Dev <akash.dev.8092@gmail.com>
2023-08-29 23:19:14 +0530

    random shapes

--- day 18/main.py
+++ day 18/main.py
@@ -9,32 +9,13 @@ colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink", "brown"]
 random.shuffle(colors)
 
 def drawShape(sides):
-    tim.color(colors.pop())
+    tim.color(colors.pop())  
     angle = 360 / sides
     for _ in range(sides):
         tim.forward(100)
         tim.right(angle)
 
-def randomWalk():
-    walk = ["left", "right", "forward", "backward"]
-    while True:
-        choice = random.choice(walk)
-        if choice == "left":
-            tim.left(90)
-        elif choice == "right":
-            tim.right(90)
-        elif choice == "forward":
-            tim.forward(50)
-        else:
-            tim.backward(50)
-        
-        if t.onkey(lambda: exit(), "Escape"):
-            break
-        
-# Listen for keyboard events
-t.listen()
-
-# Call the randomWalk() function
-randomWalk()
+for side in range(3, 11):
+    drawShape(side)
 
 t.exitonclick()
