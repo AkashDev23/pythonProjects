a1106df6121ef4cf3e74784e02c1f3a458eb0580
Akash Dev <akash.dev.8092@gmail.com>
2023-09-08 08:27:25 +0530

    hirst painting

--- day 18/hirst_painting.py
+++ day 18/hirst_painting.py
@@ -1,8 +1,36 @@
+from random import choice
 import colorgram
-
-rgb_colors = []
-colors = colorgram.extract('D:\Python Projects\day 18\image.jpg', 25)
+import turtle as t
+s = t.Screen()
+s.setup(width=500, height=500)
+rgb_list = []
+colors = colorgram.extract("D:\Python Projects\day 18\image.jpg", 25)
 for color in colors:
-    rgb_colors.append(color.rgb)
+    rgb_list.append((color.rgb.r, color.rgb.g, color.rgb.b))
 
-print(rgb_colors)
+draw = t.Turtle()  
+dot_distance = 50
+width = 10
+height = 10
+t.colormode(255)
+draw.penup()
+draw.speed("fastest")
+draw.hideturtle()
+draw.bk(200)
+draw.pensize(10)
+draw.setheading(90)
+draw.forward(170)
+draw.setheading(360)
+
+for _ in range(height):
+    for __ in range(width):
+        draw.color(choice(rgb_list))
+        draw.dot()
+        draw.forward(dot_distance)
+    draw.backward(dot_distance * width)
+    draw.right(90)
+    draw.forward(dot_distance)
+    draw.left(90)
+
+
+s.exitonclick()
