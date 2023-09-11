f1a615e1f516158b7daf9aa81ec026a88c2ec9fa
Akash Dev <akash.dev.8092@gmail.com>
2023-09-11 12:08:48 +0530

    Our snake is moving

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,8 +1,6 @@
-# main.py
-
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
-from snake import create_segments
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,20 +8,21 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-segments = create_segments()
+snake = Snake()
+
+# Listen to key events
+screen.listen()
+screen.onkey(snake.up, "Up")      
+screen.onkey(snake.down, "Down")  
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-    for seg_num in range(len(segments) - 1, 0, -1):  
-        new_x = segments[seg_num - 1].xcor()  
-        new_y = segments[seg_num - 1].ycor()  
-        segments[seg_num].goto(new_x, new_y)  
-
-    segments[0].forward(20)  
-    segments[0].left(90)     
+    
+    snake.move()
 
 screen.exitonclick()
--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -1,16 +1,54 @@
-# snake.py
-
 from turtle import Turtle
 
-def create_segments():
-    starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-    segments = []
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+move_distance = 20
 
-    for position in starting_positions:
-        new_segment = Turtle("square")
-        new_segment.color("pink")
-        new_segment.penup()
-        new_segment.goto(position)
-        segments.append(new_segment)
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+        self.direction = "" 
 
-    return segments
+    def create_snake(self):
+        for position in starting_positions:
+            new_segment = Turtle("square")
+            new_segment.color("pink")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+
+        if self.direction == "up":
+            self.head.setheading(90)
+            self.head.forward(move_distance)
+        elif self.direction == "down":
+            self.head.setheading(270)
+            self.head.forward(move_distance)
+        elif self.direction == "left":
+            self.head.setheading(180)
+            self.head.forward(move_distance)
+        elif self.direction == "right":
+            self.head.setheading(0)
+            self.head.forward(move_distance)
+
+    def up(self):
+        if self.direction != "down": 
+            self.direction = "up"
+
+    def down(self):
+        if self.direction != "up": 
+            self.direction = "down"
+
+    def left(self):
+        if self.direction != "right":
+            self.direction = "left"
+
+    def right(self):
+        if self.direction != "left": 
+            self.direction = "right"
