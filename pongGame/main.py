8f0eac53072b5e18dc697b55fe69a411a6399b99
Akash Dev <akash.dev.8092@gmail.com>
2023-09-12 23:25:14 +0530

    started building a pong game

---
+++ pongGame/main.py
@@ -0,0 +1,29 @@
+from turtle import Screen, Turtle
+from paddle import Paddle
+
+def go_up():
+    r_paddle.go_up()
+
+def go_down():
+    r_paddle.go_down()
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+screen.exitonclick()
