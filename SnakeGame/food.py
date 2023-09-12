7c5c65d93bfba280f619a804e89dbe03a166fa78
Akash Dev <akash.dev.8092@gmail.com>
2023-09-12 09:04:11 +0530

    now snake will eat turtles

--- SnakeGame/food.py
+++ SnakeGame/food.py
@@ -5,7 +5,7 @@ import random
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("#FF0000")
