f744048584a88a67fcbea7350f6af1515c228235
Akash Dev <akash.dev.8092@gmail.com>
2023-09-14 20:48:56 +0530

    pong game is completed

--- pongGame/main.py
+++ pongGame/main.py
@@ -1,36 +1,74 @@
-from turtle import Screen, Turtle
-from paddle import Paddle
-from ball import Ball
+import turtle
 import time
 
-def go_up():
-    r_paddle.go_up()
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 
-def go_down():
-    r_paddle.go_down()
 
-screen = Screen()
-screen.bgcolor("black")
-screen.setup(width=800, height=600)
+# dimensions for the game window
+WIDTH = 800
+HEIGHT = 600
+# calculate the borders, minus a little for tolerance
+x_range = int(WIDTH / 2) - 55
+y_range = int(HEIGHT / 2) - 20
+
+# screen setup
+screen = turtle.Screen()
+screen.setup(width=WIDTH, height=HEIGHT)
 screen.title("Pong")
+screen.bgcolor("black")
+# disable automatic screen updates
 screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
-ball=Ball()
+# setup
+scoreboard = Scoreboard()
+right_paddle = Paddle()
+left_paddle = Paddle(side="left")
+ball = Ball()
 
+# controls setup
 screen.listen()
-screen.onkeypress(r_paddle.go_up, "Up")
-screen.onkeypress(r_paddle.go_down, "Down")
-screen.onkeypress(l_paddle.go_up, "w")
-screen.onkeypress(l_paddle.go_down, "s")
+screen.onkeypress(right_paddle.move_up, "Up")
+screen.onkeypress(right_paddle.move_down, "Down")
+screen.onkeypress(left_paddle.move_up, "w")
+screen.onkeypress(left_paddle.move_down, "s")
 
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
+game_is_over = False
+while not game_is_over:
     screen.update()
+    time.sleep(ball.move_speed)
     ball.move()
-    if ball.ycor()>280 or ball.ycor()<-280:
-        ball.bounce()
 
+    # detect collision with top and bottom walls
+    if ball.ycor() < -y_range or ball.ycor() > y_range:
+        ball.wall_bounce()
+
+    # detect collision with each paddle
+    # ball.distance() is unreliable since it measures from the center point of the paddle
+    # better to check the coordinates separately
+    if x_range - ball.xcor() < 10 and ball.xcor() < x_range + 20 and abs(right_paddle.ycor() - ball.ycor()) < 50:
+        ball.paddle_bounce()
+    elif abs(x_range + ball.xcor()) < 10 and ball.xcor() > -x_range - 20 and abs(left_paddle.ycor() - ball.ycor()) < 50:
+        ball.paddle_bounce()
+
+    # detect if ball completely passes the right or left side
+    if ball.xcor() > WIDTH / 2 + 30:
+        # update score
+        scoreboard.add_one_left()
+        # reset ball position and heading
+        ball.reset_position()
+    elif ball.xcor() < WIDTH / -2 - 30:
+        scoreboard.add_one_right()
+        # reset ball position and set the heading to the left
+        ball.reset_position(side="left")
+
+    # game over condition
+    if scoreboard.left_score == 5 or scoreboard.right_score == 5:
+        game_is_over = True
+
+# display the game over message
+scoreboard.game_over()
+
+# wait for click before closing the window
 screen.exitonclick()
--- pongGame/paddle.py
+++ pongGame/paddle.py
@@ -1,18 +1,33 @@
-from turtle import Turtle
+import turtle
 
-class Paddle(Turtle):
-    def __init__(self, position):
+MOVE_DISTANCE = 20
+
+
+class Paddle(turtle.Turtle):
+    def __init__(self, side=""):
+        """Creates a paddle. Defaults to the right side, unless "left" is specified."""
         super().__init__()
+        # calculate from the screen size, offset by 5% from the edge
+        self.x_pos = int(self.getscreen().window_width() * 45 / 100)
+        # multiply by -1 to get the left paddle
+        if side == "left":
+            self.x_pos *= -1
         self.shape("square")
-        self.color("pink")
-        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        # default size is 20x20, this will make it 20x100
+        self.shapesize(stretch_len=5)
         self.penup()
-        self.goto(position)
+        self.speed(0)
+        # face to the to the top of the screen, so we can use .forward() and .backward()
+        self.setheading(90)
+        # start in the middle, so y_pos=0
+        self.setpos(self.x_pos, 0)
 
-    def go_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+    def move_up(self):
+        """Moves the paddle up."""
+        # simpler to just make use of the inherited methods
+        self.forward(MOVE_DISTANCE)
 
-    def go_down(self):
-        new_y = self.ycor() - 20
-        self.goto(self.xcor(), new_y)
+    def move_down(self):
+        """Moves the paddle down."""
+        self.backward(MOVE_DISTANCE)
