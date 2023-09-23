d00a965da8491c88b937c2da489a7818aaa85130
Akash Dev <akash.dev.8092@gmail.com>
2023-09-23 09:49:11 +0530

    highscore in snake game

--- SnakeGame/score.py
+++ SnakeGame/score.py
@@ -4,6 +4,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()  
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -13,7 +14,13 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Courier", 24, "normal"))
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Courier", 24, "normal"))
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score=self.score
+        self.score=0
+        self.update_scoreboard()
 
     def game_over(self):
         self.goto(0, 0)
